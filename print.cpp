e7cc1b9fa27cd712a4d6e4b19ecd9f3e5b1b6bcd
Muskan645gupta <51221607+Muskan645gupta@users.noreply.github.com>
2020-09-18 21:34:34 +0530

    Create print.cpp

---
+++ print.cpp
@@ -0,0 +1,13 @@
+//print z pattern in c++ using cout
+#include<iostream>
+using namespace std;
+int main()
+{   
+    cout<<"*****"<<endl;
+    cout<<"   *"<<endl;
+    cout<<"  *"<<endl;
+    cout<<" *"<<endl;
+    cout<<"*****"<<endl;
+    
+    return 0;
+}
